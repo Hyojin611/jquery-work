0024ffdeb651089339b26fd768035355b2ac93f6
Hyojin611 <611gywls@gmail.com>
2025-02-13 10:42:42 +0900

    forLoop

--- js/10.js
+++ js/10.js
@@ -6,6 +6,51 @@ $(document).ready(function(){
     { name : '레이', age :22},
     { name : '이서', age :18},
     { name : '가을', age :23},
-
   ]
+
+  $("#btnforLoop").on("click", function(){
+    let result = ''
+    //for 구문
+    for(let i = 0; i < data.length; i++){
+      result = result + `
+      <tr>
+          <td>${i+1}</td>
+          <td>${data[i].name}</td>
+          <td>${data[i].age}</td>
+        </tr>
+        `
+    }
+    $("#forOut").html(result)
+  })
+
+
+  $("#btnforEach").on("click",function(){
+    let result = '';
+    //for~Each 구문
+    data.forEach((x,index)=>{
+      result = result + `
+      <tr>
+          <td>${index+1}</td>
+          <td>${x.name}</td>
+          <td>${x.age}</td>
+        </tr>
+        `
+    })
+    $("#forEach").html(result)
+  })
+
+  $("#btnJQuerEach").on("click",function(){
+    let result = '';
+
+    $.each(data,(index,x)=>{
+      result = result + `
+      <tr>
+          <td>${index+1}</td>
+          <td>${x.name}</td>
+          <td>${x.age}</td>
+        </tr>
+        `
+    })
+    $("#jqueryEach").html(result)
+  })
 })
